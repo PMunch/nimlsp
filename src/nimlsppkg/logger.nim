113c895a0fd2c84ce06fc9886aad28355a7e522b
bung87 <crc32@qq.com>
2022-01-31 00:01:27 +0800

    use file logger

---
+++ src/nimlsppkg/logger.nim
@@ -0,0 +1,22 @@
+import os,logging
+
+let rollingLog = newRollingFileLogger( getAppDir() / "nimlsp.log")
+addHandler(rollingLog)
+
+template debugLog*(args: varargs[string, `$`]) =
+  when defined(debugLogging):
+    debug join(args)
+    flushFile rollingLog.file
+
+template infoLog*(args: varargs[string, `$`]) =
+  when defined(debugLogging):
+    info join(args)
+    flushFile rollingLog.file
+
+template errorLog*(args: varargs[string, `$`]) =
+  when defined(debugLogging):
+    error join(args)
+
+template warnLog*(args: varargs[string, `$`]) =
+  when defined(debugLogging):
+    warn join(args)
